9f9a546f5bd4c835698086e00d23379d3ffa111d
surferconor425 <conorcodedev@outlook.com>
2016-05-07 14:38:18 +0100

    Added tooltip for Chemical Formula

--- src/main/java/com/surferconor425/conorco/item/dustPotassiumNitrate.java
+++ src/main/java/com/surferconor425/conorco/item/dustPotassiumNitrate.java
@@ -5,8 +5,11 @@ import cofh.lib.util.OreDictionaryProxy;
 import cofh.lib.util.helpers.ItemHelper;
 import cofh.lib.util.helpers.StringHelper;
 import com.surferconor425.conorco.reference.ItemRef;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.List;
 
 
 /**
@@ -19,5 +22,9 @@ public class dustPotassiumNitrate extends ItemConorCo
         this.setUnlocalizedName(ItemRef.Dusts.dust1);
         ItemHelper.isDust(dustPotassiumNitrate);
 
+    }
+    public void addInformation(ItemStack item, EntityPlayer player, List list, boolean isInHand)
+    {
+        list.add("KNO3");
     }
 }
