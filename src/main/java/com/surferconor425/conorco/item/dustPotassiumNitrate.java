66bc7c1ba9e017941d5b85f603cb8bc26a794664
surferconor425 <conorcodedev@outlook.com>
2016-05-07 15:21:40 +0100

    Ditched random imports

--- src/main/java/com/surferconor425/conorco/item/dustPotassiumNitrate.java
+++ src/main/java/com/surferconor425/conorco/item/dustPotassiumNitrate.java
@@ -1,14 +1,9 @@
 package com.surferconor425.conorco.item;
 
-import cofh.lib.gui.element.ElementSimpleToolTip;
-import cofh.lib.util.OreDictionaryProxy;
 import cofh.lib.util.helpers.ItemHelper;
-import cofh.lib.util.helpers.StringHelper;
 import com.surferconor425.conorco.reference.ItemRef;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
-
 import java.util.List;
 
 
